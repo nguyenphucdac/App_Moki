51a3b2141914ca9ce10b7ae1f93b5cc0619fc4df
nguyenphucdac <nguyenphucdac@gmail.com>
2017-10-20 23:59:55 +0700

    commit add page search

---
+++ app/src/main/java/com/example/dac/app_moki/view/search/Search_Activity.java
@@ -0,0 +1,16 @@
+package com.example.dac.app_moki.view.search;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.dac.app_moki.R;
+
+/**
+ * Created by Dac on 10/20/2017.
+ */
+public class Search_Activity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_layout);
+    }
+}
